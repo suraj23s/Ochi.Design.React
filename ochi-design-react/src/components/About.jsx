1799d61da4e6fc037e48fc556ae8cae09086d3e8
suraj232000suman <surajsuman232000@gmail.com>
2025-12-17 23:15:33 +0530

    about section completd

--- ochi-design-react/src/components/About.jsx
+++ ochi-design-react/src/components/About.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 function About() {
@@ -10,7 +11,7 @@ function About() {
         </div>
         <div className='basis-1/2 flex gap-30'>
           <div className='basis-1/2'>
-             <p>We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it's live or digital, delivered for one or a hundred people.</p>
+             <p>We create <motion.span>tailored presentations</motion.span> to help you persuade your colleagues, clients, or investors. Whether it's live or digital, delivered for one or a hundred people.</p>
             <p className='mt-10'>We believe the mix of strategy and design with a bit of coffie is what makes your message clear, convincing, and captivating.</p>
           </div>
           <div className='basis-1/2 flex-end'>
@@ -25,6 +26,15 @@ function About() {
           </div>
         </div>
       </div>
+       <div className="w-full border-t-[1px] pt-10 mt-20 borderd-[#a1b562] flex gap-5">
+        <div className='w-1/2'>
+          <h1 className='text-7xl'>Our approach:</h1>
+          <button className='flex gap-10 items-center px-10 py-6 bg-zinc-900 rounded-full text-white mt-10'>Read More
+            <div className='w-3 h-3 bg-zinc-100 rounded-full'></div>
+          </button>
+        </div>
+        <img src='https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-1326x939.jpg' className='w-1/2 h-[70vh] bg-[#b0c859] rounded-3xl'></img>
+       </div>
     </div>
   )
 }
